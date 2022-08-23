1a1bb4206e030e907168a79a5b4e06596b7bd3b2
yourkeksuk <igor-18css@mail.ru>
2022-08-23 21:55:13 +0300

    Test commit

---
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -0,0 +1,64 @@
+package ru.netology.javaqa;
+
+public class Radio {
+
+    private int numberStation;
+    private int volume;
+
+    public int getNumberStation() {
+        return numberStation;
+    }
+
+    public void setNumberStation(int numberStation) {
+        if (numberStation < 0) {
+            return;
+        }
+        if (numberStation > 9) {
+            return;
+        }
+        this.numberStation = numberStation;
+    }
+
+    public void next() {
+        if (numberStation == 9) {
+            numberStation = 0;
+        } else {
+            numberStation++;
+        }
+    }
+
+    public void prev() {
+        if (numberStation == 0) {
+            numberStation = 9;
+        } else {
+            numberStation--;
+        }
+    }
+
+    public int getVolume() {
+        return numberStation;
+    }
+
+    public void setVolume(int volume) {
+        if (volume < 0) {
+            return;
+        }
+        if (volume > 10) {
+            return;
+        }
+        this.volume = volume;
+    }
+
+    public void plusVolume() {
+        if (volume < 10) {
+            volume++;
+        }
+    }
+
+    public void minusVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+
+}
---
+++ src/test/java/ru/netology/javaqa/RadioTest.java
@@ -0,0 +1,41 @@
+package ru.netology.javaqa;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class RadioTest {
+
+    @Test
+    public void setNumberStation(){
+        Radio radio = new Radio();
+        radio.setNumberStation(10);
+
+        int expected = 0;
+        int actual = radio.getNumberStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void next(){
+        Radio radio = new Radio();
+        radio.setNumberStation(9);
+        radio.next();
+
+        int expected = 0;
+        int actual = radio.getNumberStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prev(){
+        Radio radio = new Radio();
+        radio.setNumberStation(0);
+        radio.prev();
+
+        int expected = 9;
+        int actual = radio.getNumberStation();
+        Assertions.assertEquals(expected, actual);
+    }
+}
